ee7c37403e749ab406453d4eca424cc5da25f615
Adellar Irankunda <addyirankunda@gmail.com>
2024-03-26 13:25:29 -0400

    pipeline setup pt.1

---
+++ Encoder.swift
@@ -0,0 +1,50 @@
+//
+//  Encoder.swift
+//  Pathtracer
+//
+//  Created by Adellar Irankunda on 3/26/24.
+//
+
+import Metal
+
+final class UniformsSetter{
+    var tint: Float = .zero
+    private var deviceSupportsNonuniformThreadgroups: Bool
+    private let pipelineState: MTLComputePipelineState
+    
+    
+    init(library: MTLLibrary) throws{
+        self.deviceSupportsNonuniformThreadgroups = library.device.supportsFeatureSet(.iOS_GPUFamily4_v1)
+        let constantValues = MTLFunctionConstantValues()
+        constantValues.setConstantValue(&self.deviceSupportsNonuniformThreadgroups, type: .bool, index: 0)
+        let function = try library.makeFunction(name: "main", constantValues: constantValues)
+        self.pipelineState = try library.device.makeComputePipelineState(function: function)
+    }
+    
+    func encode(source: MTLTexture, destination: MTLTexture, in commandBuffer: MTLCommandBuffer){
+        guard let encoder = commandBuffer.makeComputeCommandEncoder()
+        else{ return}
+        
+        encoder.label = "Pathtracer"
+        encoder.setTexture(source, index: 0)
+        encoder.setTexture(destination, index: 1)
+        encoder.setBytes(&self.tint, length: MemoryLayout<Float>.stride, index: 0)
+        
+        let threadDimensions = MTLSize(width: source.width, height: source.height, depth: 1)
+        let threadGroupsX = self.pipelineState.threadExecutionWidth
+        let threadGroupsY = self.pipelineState.maxTotalThreadsPerThreadgroup / threadGroupsX
+        let threadGroupSize = MTLSize(width: threadGroupsX, height: threadGroupsY, depth: 1)
+        
+        encoder.setComputePipelineState(self.pipelineState)
+        if self.deviceSupportsNonuniformThreadgroups {
+            encoder.dispatchThreadgroups(threadDimensions, threadsPerThreadgroup: threadGroupSize)
+        }
+        else {
+            let threadGroupCount = MTLSize(width: (threadDimensions.width + threadGroupSize.width - 1) / threadGroupSize.width, height: (threadDimensions.height + threadGroupSize.height - 1) / threadGroupSize.height, depth: 1)
+            
+            encoder.dispatchThreadgroups(threadGroupCount, threadsPerThreadgroup: threadGroupSize)
+        }
+        
+        encoder.endEncoding()
+    }
+}
--- Pathtracer/ViewController.swift
+++ Pathtracer/ViewController.swift
@@ -6,4 +6,54 @@
 //
 
 import Foundation
-import 
+import UIKit
+import Metal
+
+class ViewController: UIViewController {
+    
+    enum Error: Swift.Error {
+        case commandQueuereationFailed
+    }
+    
+    private let device: MTLDevice
+    private let encoder: UniformsSetter
+    private let imageView: UIImageView
+    private let commandQueue: MTLCommandQueue
+    private var texturePair: (source: MTLTexture, destination: MTLTexture)?
+    
+    
+    init(device: MTLDevice) throws{
+        let library = try device.makeDefaultLibrary(bundle: .main)
+        guard let commandQueue = device.makeCommandQueue()
+        else { throw Error.commandQueuereationFailed}
+        self.device = device
+        self.commandQueue = commandQueue
+        self.imageView = .init()
+        super.init(nibName: nil, bundle: nil)
+        
+    }
+    
+    
+    required init?(coder: NSCoder){
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    private func redraw() {
+        guard let source = self.texturePair?.source,
+              let destination = self.texturePair?.destination,
+              let commandBuffer = self.commandQueue.makeCommandBuffer()
+        else { return }
+        
+        self.encoder.encode(source: source, destination: destination, in: commandBuffer)
+        
+        commandBuffer.addCompletedHandler { _ in
+            guard let cgImage = try? self.
+        }
+        
+        
+    }
+    
+    
+}
