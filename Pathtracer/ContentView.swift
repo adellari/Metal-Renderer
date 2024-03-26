d810ee8a8e214e6b6384d98d1c4c8b040cacd67b
Adellar Irankunda <addyirankunda@gmail.com>
2024-03-26 10:46:15 -0400

    Initial Commit

---
+++ Pathtracer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pathtracer
+//
+//  Created by Adellar Irankunda on 3/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pathtracer/PathtracerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathtracerApp.swift
+//  Pathtracer
+//
+//  Created by Adellar Irankunda on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct PathtracerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
