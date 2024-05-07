a5f74e74054bbacc201fd281e39de86d41260b4e
Adellar Irankunda <addyirankunda@gmail.com>
2024-05-06 23:59:19 -0700

    Starting denoiser wrapper integration

---
+++ OIDNBridge.h
@@ -0,0 +1,13 @@
+//
+//  OIDNBridge.h
+//  Pathtracer
+//
+//  Created by Adellar Irankunda on 5/6/24.
+//
+
+#ifndef OIDNBridge_h
+#define OIDNBridge_h
+#include <OpenImageDenoise>
+#include <OpenImageDenoise/oidn.hpp>
+
+#endif /* OIDNBridge_h */
