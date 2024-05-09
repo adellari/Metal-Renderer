23e8d50db1630ebfdc5c52c7b2489657ddbf9604
Adellar Irankunda <addyirankunda@gmail.com>
2024-05-08 18:52:59 -0700

    Built OIDN for iphoneos and iphonesimulator

---
+++ OIDN-Bridge.h
@@ -0,0 +1,25 @@
+//
+//  OIDN-Bridge.h
+//  Pathtracer
+//
+//  Created by Adellar Irankunda on 5/7/24.
+//
+
+#ifndef OIDN_Bridge_h
+#define OIDN_Bridge_h
+
+//#import <OpenImageDenoise>
+#import "OpenImageDenoise/oidn.h"
+/*
+#import "oidn.h"
+
+OIDNDevice device;
+
+void initializeDevice()
+{
+    device = oidnNewDevice(OIDN_DEVICE_TYPE_METAL);
+    oidnCommitDevice(device);
+}
+
+*/
+#endif /* OIDN_Bridge_h */
